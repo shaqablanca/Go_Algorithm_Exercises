b5bfd238dfb687f212eb31428e7f386faece3fce
shaqablanca <a.sameduz@gmail.com>
2022-02-27 23:24:03 +0100

    Find the Prime Number

---
+++ PrimeNumber/prime.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+ func yes(int k) bool{
+if k == 1 || k==0{
+	return false
+}
+
+var arr [100]int
+
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i + 1
+
+	}
+
+for i:=0 ; i<
+// }
+
+func main() {
+
+	
+
+	 for s := 0; s <= len(arr); s++ {
+	 	if arr[s] > 1 {
+	 		for i := 2; i <= (s / 2); i++ {
+ 			if s%i == 0 {
+	// 				yes = 1
+
+	// 			}
+	// 		}
+	// 		if yes == 0 && arr[s] != 1 {
+	// 			fmt.Println(arr[s])
+	// 		}
+
+	// if arr[s]%2 != 0 && arr[s]%3 != 0 && arr[s]%5 != 0 && arr[s]%7 != 0 {
+	// 	//fmt.Printf("%d\n is not a prime number",arr[i])
+	// 	fmt.Println(arr[s])
+
+	// }
+
+	//}
+
+	//}
+}
